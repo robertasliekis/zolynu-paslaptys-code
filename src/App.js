2f9bad4fabb52d824ee36c26918dc4b4b432f0ff
robertasliekis <60491171+robertasliekis@users.noreply.github.com>
2020-10-16 18:34:14 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React from "react";
-import Page1 from "./components/Page1";
-import VideoBackground from "./components/VideoBackground";
 import InfoContainers from "./components/InfoContainers";
 import ChangePageButtons from "./components/ChangePageButtons";
 import { connect } from "react-redux";
 
 import "./App.css";
 //"homepage": "http://https://robertasliekis.github.io/zolynu-paslaptys/",
+//"homepage": "http://localhost:3000/",
+//
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activePage: 0,
-    };
+  addClassAnimation(number) {
+    if (number === this.props.pageNumber) {
+      return "page-wrapper-animation";
+    } else {
+      return "";
+    }
   }
 
   render() {
+    const pageCount = [0, 1, 2, 3, 4, 5, 6, 7];
+
     return (
       <div className="App">
         <div className="content-wrapper">
           <ChangePageButtons />
-          <VideoBackground />
-          {/* <div
-            className="page1-div"
-           // style={{ display: this.props.pageNumber === 1 ? "flex" : "none" }}
-          >
-            <Page1 />
-          </div> */}
-          <div
-            className="info-container-div"
-            // style={{ display: this.props.pageNumber === 2 ? "flex" : "none" }}
-          >
-            <InfoContainers />
-          </div>
+          <div className={"page-wrapper page-start " + this.addClassAnimation(-1)}></div>
+          {pageCount.map((page, index) => {
+            return (
+              <div
+                key={index}
+                className={
+                  "page-wrapper plant-page page" + page + " " + this.addClassAnimation(page)
+                }
+              >
+                <InfoContainers page={page} />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
